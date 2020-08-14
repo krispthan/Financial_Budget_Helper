9fb433e887f1a369b55a4e1f44209b1a9594cf5e
Christine Than <krispthan@gmail>
2020-08-13 21:01:57 -0700

    retrieve input data from DOM and add even handler

--- script.js
+++ script.js
@@ -1,26 +1,49 @@
 //Budget Controller Module
 
-let budgetController = (() => {
-  let x = 50;
-  let add = (a) => {
-    return x + a;
+let budgetController = (() => {})();
+
+//UI Controller Module
+let UIController = (() => {
+  let DOMstrings = {
+    inputType: '.add__type',
+    inputDescription: '.add__description',
+    inputValue: '.add__value',
+    inputBtn: '.add__btn',
   };
   return {
-    publicTest: (b) => {
-      return add(b);
+    getInput: () => {
+      return {
+        type: document.querySelector(DOMstrings.inputType).value, // income or expense
+        description: document.querySelector(DOMstrings.inputDescription).value,
+        value: document.querySelector(DOMstrings.inputValue).value,
+      };
+    },
+    getDomStrings: () => {
+      return DOMstrings;
     },
   };
 })();
 
-//UI Controller Module
-let UIController = (() => {})();
-
 //App Controller Module
 let AppController = ((budgetCtrl, UIContrl) => {
-  let z = budgetCtrl.publicTest(5);
-  return {
-    anotherPublic: () => {
-      console.log(z);
-    },
+  let DOM = UIContrl.getDomStrings();
+
+  let ctrlAddItem = () => {
+    //1. Get input data from field
+    let input = UIController.getInput();
+    console.log(input);
+
+    //2. Add the item to the budget controller
+    //3. Add the item to the UI
+    //4. Calculate the budget
+    //5. Display budget to UI
   };
+
+  document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
+
+  document.addEventListener('keypress', (event) => {
+    if (event.keyCode === 13 || event.which === 13) {
+      ctrlAddItem();
+    }
+  });
 })(budgetController, UIController);
